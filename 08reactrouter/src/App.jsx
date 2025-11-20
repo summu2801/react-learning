b7f4afa2e384527b08cb0761ba8767b176e2efa9
hkv28032005 <hkvr28032005@gmail.com>
2025-11-20 21:54:51 +0530

    my react router

---
+++ 08reactrouter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+import Header from './components/header/header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ 08reactrouter/src/components/User/User.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+export default function User(){
+    const {userid}=useParams()
+    return(
+        <div>User: {userid}</div>
+    )
+}
