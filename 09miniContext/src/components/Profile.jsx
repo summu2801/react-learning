6540b8f657bea4c48b6c3f1d74964c4ad0edcd76
hkv28032005 <hkvr28032005@gmail.com>
2025-11-21 19:48:44 +0530

    understand contextapi through small project

---
+++ 09miniContext/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import React,{useContext,useState}from "react";
+import UserContext from "../context/UserContext";
+
+function Profile(){
+    const {user}=useContext(UserContext)
+    if(!user)return <div>Please Login</div>
+    return <div>Welcome {user.username}</div>
+}
+export default Profile
