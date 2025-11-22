9695448bbcfd6ebdeca69a4cdac7e14121e28f6b
hkv28032005 <hkvr28032005@gmail.com>
2025-11-22 11:24:41 +0530

    learn another variant of context api

---
+++ 10themeswitcher/src/contexts/theme.js
@@ -0,0 +1,17 @@
+import { createContext,useContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode:"light",
+    darkTheme:()=>{},
+    lightTheme:()=>{},
+})
+
+export const ThemeProvider=ThemeContext.Provider
+
+//custom hooks
+export default function useTheme(){
+    return useContext(ThemeContext)
+
+}
+
+//only need to import useTheme in other file
